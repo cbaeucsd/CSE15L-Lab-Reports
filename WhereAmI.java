3bd94d059a2723dd1a07b7ccacd1e13dbba4ba36
cbaeucsd <cbae@ucsd.edu>
2022-04-05 23:18:55 -0700

    Challenge

--- WhereAmI.java
+++ WhereAmI.java
@@ -1,5 +1,6 @@
 class WhereAmI {
   public static void main(String[] args) {
+    System.out.println("edit1");
     System.out.println(System.getProperty("os.name"));
     System.out.println(System.getProperty("user.name"));
     System.out.println(System.getProperty("user.home"));
