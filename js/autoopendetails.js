2ef157a1c534759d619f529f08b0ac055491a0fe
cbaeucsd <102708712+cbaeucsd@users.noreply.github.com>
2022-04-07 11:44:34 -0700

    Update autoopendetails.js

--- js/autoopendetails.js
+++ js/autoopendetails.js
@@ -1,41 +1,10 @@
-function MakeArrayOfAllPrefixes(str){
-  //console.log("MakeArrayOfAllPrefixes("+str+")");
-  var prefixes = [];
-  for (var i=1; i<=str.length; i++){
-prefixes.push(str.substr(0,i));
-  }
-  console.log("MakeArrayOfAllPrefixes("+str+") -> returns [" + prefixes + "]");
-  return prefixes;
+const openDetailsIfAnchorHidden = evt => {
+  const targetDIV = document.querySelector(evt.target.getAttribute("href"));
+  if ( !! targetDIV.offsetHeight || targetDIV.getClientRects().length ) return;
+  targetDIV.closest("details").open = true;
 }
 
-function SetOpenAttrForIdsAndPrefixes(ids, openAttrBool){
-  $('details').attr('open',false); // close all others first
-  console.log("SetOpenAttrForIds([" +ids+"], "+openAttrBool+")");
-  for (idindex in ids) {
-var id = ids[idindex];
-if (id != ""){
-  var prefixes = MakeArrayOfAllPrefixes(id);
-  for (prefixidx in prefixes) {
-    var prefix = prefixes[prefixidx];
-    if(openAttrBool==true) { operationstr="Opening"; } else { operationstr="Closing"};
-    console.log(operationstr+" <details id='#"+prefix+"'> with $('#"+prefix+").attr('open',"+openAttrBool+");");
-    $('#'+prefix).attr('open',openAttrBool);
-  }
-}
-  }
-}
 
-function SetOpenAttrForIdsAndPrefixesFromLocationHash(){
-  var hashes = $(location).attr('hash').split('#'); 
-  SetOpenAttrForIdsAndPrefixes(hashes, true); 
-}
-
-$(document).ready(function(){
-  SetOpenAttrForIdsAndPrefixesFromLocationHash();
-
-  if ("onhashchange" in window) {// does the browser support the hashchange event?
-  window.onhashchange = function () {
-     SetOpenAttrForIdsAndPrefixesFromLocationHash();
-  }
-  }
-});
+[...document.querySelectorAll("[href^='#']")].forEach( 
+   el => el.addEventListener("click", openDetailsIfAnchorHidden )
+);
